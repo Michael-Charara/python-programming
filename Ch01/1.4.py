cedf0de85bf7e21f4317c72fa2dbf7774a9cdb8e
Michael Charara <66533339+Michael-Charara@users.noreply.github.com>
2021-03-16 17:19:39 -0400

    chapter 1 programs

---
+++ Ch01/1.4.py
@@ -0,0 +1,11 @@
+#  Draw a 5−pointed star
+import turtle
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
--- Ch01_ex09.py
+++ Ch01_ex09.py
@@ -1,2 +1,11 @@
-#promt the user to enter a number
-number_str = input("Enter a number:")
+# Promt the user for a number number
+number_str = input("Enter a number: ")
+# Store the given number into a variable
+number = int(number_str)
+# Perform arithmetic operations with the given number
+# take numver, add 2, multiply by 3, subtraact 6, and divide by 3
+n = (((number + 2)*3)-6)/3  # division
+# display the values
+print('Number Entered: ', number)
+# display the result
+print('Result: ', n)
