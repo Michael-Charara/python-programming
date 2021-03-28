c05630117e46de0f86228b7d56bd533be5444808
Michael Charara <66533339+Michael-Charara@users.noreply.github.com>
2021-03-27 22:24:27 -0400

    ch2 ex 20

---
+++ Module2/Ch02_ex20.py
@@ -0,0 +1,17 @@
+# Write a program that prompts for an integer and prints the integer, but if something other than an integer is input, the program keeps asking for an integer. Here is a sample session:
+# Input an integer: abc
+# Error: try again. Input an integer: 4a Error: try again. Input an integer: 2.5 Error: try again. # Input an integer: 123 The integer is: 123
+# Hint: The string isdigit method will be useful.
+
+# Get integer input
+num = input("Enter an integer number: ")
+
+# Look executes until input is other than integer
+while num.isdigit() == false:
+    # display the error message and get another input
+    num = input("Error: trye again, Enter an integer number: ")
+else:
+    # convert the input to integer
+    number_int = int(num)
+    # Display the integer number
+    print("The integer number is: ", number_int)
